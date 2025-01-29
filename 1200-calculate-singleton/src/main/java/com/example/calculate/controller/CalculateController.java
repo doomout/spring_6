b8cb7bd24ccdf0add14402dfd13e7aeb7260f18c
khg <winquake3@hanmail.net>
2025-01-29 21:18:06 +0900

    싱글톤과 스레드 세이프 코드

--- 1200-calculate-singleton/src/main/java/com/example/calculate/controller/CalculateController.java
+++ 1200-calculate-singleton/src/main/java/com/example/calculate/controller/CalculateController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalculateController {
-
-    private int total;
+    //전역 변수는 다중 스레드에서 에러 발생
+    //private int total;
 
 	@GetMapping("/calculate-price")
 	public String calculatePrice(@RequestParam int a, @RequestParam int b, @RequestParam int c, Model model) {
-        total = a + b + c;
+        //지역 변수로 안전하게 에러 제거
+        int total = a + b + c;
         System.out.println(total);
         int price = (int)(total * 1.1);
         model.addAttribute("price", price);
