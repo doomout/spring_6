68c753f25f6e945c03c0d95bd2924c81deecd23d
khg <winquake3@hanmail.net>
2025-02-09 22:01:17 +0900

    최소한 검색 결과가 예상대로 나오는지 확인

---
+++ 2002-shopping-test-service-repository/src/test/java/com/example/shopping/service/CatalogServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.shopping.service;
+
+import com.example.shopping.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//Service, Repository 통합 테스트기에 스프링 MVC 구동을 위한 웹 관련 설정이 필요 없다.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional //@SpringBootTest 에는 트렌젝션이 미포함이라 명시적으로 설정해야 한다.
+@Sql("CatalogServiceTest.sql") //테스트를 위한 데이터 인서트
+public class CatalogServiceTest {
+    @Autowired
+    CatalogService catalogService;
+
+    //최소한 검색 결과가 예상대로 나오는지 확인
+    @Test
+    void test_findAll() {
+        List<Product> products = catalogService.findAll();
+        assertThat(products.size()).isEqualTo(5);
+    }
+}
